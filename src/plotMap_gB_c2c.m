fde3a593d5e0012bd713f2b163c78ebdb055e5ff
frannie <frannie@mek.dtu.dk>
2021-02-22 15:51:58 +0100

    added plotMap_gB_c2c

---
+++ src/plotMap_gB_c2c.m
@@ -0,0 +1,46 @@
+function plotMap_gB_c2c(job,varargin)
+%
+% Syntax
+%
+%  plotMap_gB_c2c(job,varargin)
+%
+% Input
+%  job  - @parentGrainreconstructor
+
+gB_c2c = job.grains.boundary(job.csChild.mineral,job.csChild.mineral);
+if ~isempty(gB_c2c)
+    %% Plot the parent-child misorientation distribution map
+    fprintf(' -> Plotting the child-child misorientation distribution map');
+    f = figure;
+    job.ebsd = swapColors(job.ebsd,'gray');
+    plot(job.grains);
+    hold on
+    % Plot the HABs in black
+    plot(job.grains.boundary,'LineColor','k','displayName','HABs',varargin{:});
+    hold on
+    % Plot the LABs boundaries in navajowhite
+    plot(job.grains.innerBoundary,'LineColor',[255/255 222/255 173/255],'displayName','LABs',varargin{:});
+    hold on
+    % Plot the IPBs in jet scale
+    plot(gB_c2c,gB_c2c.misorientation.angle./degree,varargin{:})
+    hold off
+    
+    % Round-off the maximum number of color levels to the nearest 5 degrees
+    fR = fundamentalRegion(job.csParent,job.csChild);
+    maxColors = ceil((fR.maxAngle/degree)/5)*5;
+    colormap(jet(maxColors))
+    caxis([0 maxColors]);
+    colorbar('location','eastOutSide','LineWidth',1.25,'TickLength', 0.01,...
+        'YTick', [0:5:maxColors],...
+        'YTickLabel',num2str([0:5:maxColors]'), 'YLim', [0 maxColors],...
+        'TickLabelInterpreter','latex','FontName','Helvetica','FontSize',14,'FontWeight','bold');
+    set(f,'Name','Child-child boundary misorientation map','NumberTitle','on');
+    drawnow;
+else
+    message = sprintf('Plotting aborted: No child-child boundaries found');
+    uiwait(errordlg(message));
+    error('Child-child misorientation distribution map empty');
+end
+job.ebsd = swapColors(job.ebsd,'RGB');
+end
+
