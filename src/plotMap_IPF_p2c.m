53c8b9f2b11297eddd560e02726c1da8acb2282a
frannie <frannie@mek.dtu.dk>
2021-02-23 11:36:31 +0100

    bugfix plotMap_IPF_p2c

--- src/plotMap_IPF_p2c.m
+++ src/plotMap_IPF_p2c.m
@@ -13,6 +13,8 @@ function ipfKey = plotMap_IPF_p2c(job, varargin)
 %  ipfKey       - @ipfHSVKey
 
 vector = getClass(varargin,'vector3d',vector3d.X);
+ipfKey1 = [];
+ipfKey2 = [];
 
 if ~isempty(job.ebsd(job.csParent))
     f = figure;
@@ -48,25 +50,3 @@ else
 end
 ipfKey = [ipfKey1, ipfKey2];
 end
-
-% f = figure;
-% newMtexFigure('nrows',1,'ncols',2);
-% phaseType = {'Parent','Child'};
-% phase = {job.csParent,job.csChild};
-% % cycle through all components of the tensor
-% for ii = 1:2
-%     nextAxis(1,ii)
-%
-%     if ~isempty(job.ebsd(phase{ii}))
-%         ipfKey{ii} = ipfHSVKey(job.ebsd(phase{ii}));
-%         colors = ipfKey{ii}.orientation2color(job.ebsd(phase{ii}).orientations);
-%         plot(job.ebsd(phase{ii}),colors,'figSize','large');
-%     end
-%
-%     hold on
-%     plot(job.grains.boundary,'linewidth',1);
-%     hold off
-%     title([phaseType{ii},': ',phase{ii}.mineral]);
-% end
-% set(f,'Name','Parent and child IPF maps','NumberTitle','on');
-% drawNow(gcm,'figSize','large');
