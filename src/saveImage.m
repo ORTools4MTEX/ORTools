67a4e0133ef27170db6b85b1a0b067feb5c20a6d
frannie <frannie@mek.dtu.dk>
2021-03-08 15:52:17 +0100

    updated saveImage

--- src/saveImage.m
+++ src/saveImage.m
@@ -1,4 +1,15 @@
-function saveImage
+function saveImage(fileDir,fileName)
+% Save all open figures
+%
+% Syntax
+% saveImage(fileDir,fileName)
+% saveImage(fileDir)
+% saveImage
+%
+% Input
+%  fileName    - file name (string with file of type *.bmp, *.jpeg, *.png or *.tiff )
+%  fileDir     - file directory
+
 
 % Compute the number of figure windows currently open
 openFigs = get(0,'Children');
@@ -7,15 +18,19 @@ if numOpenFigs == 0
     error('Image save terminated: No open figures');
     
 else % Figure windows are open
-    currentFolder = pwd;
-    [fileNameType,~,fileIdx] = uiputfile(...
-        {'*.bmp','Microsoft Windows Bitmap';...
-        '*.jpeg','Joint Photographic Experts Group';...
-        '*.png','Portable Network Graphics';...
-        '*.tiff','Tagged Image File Format';...
-        '*.*','All files (*.*)'},...
-        'Save file',[currentFolder,'\','*.png']);
-    
+    if nargin == 2
+        fileNameType = fileName; 
+        fileIdx = 3;
+    else
+        if nargin == 0; fileDir = pwd; end
+        [fileNameType,fileDir,fileIdx] = uiputfile(...
+            {'*.bmp','Microsoft Windows Bitmap';...
+            '*.jpeg','Joint Photographic Experts Group';...
+            '*.png','Portable Network Graphics';...
+            '*.tiff','Tagged Image File Format';...
+            '*.*','All files (*.*)'},...
+            'Save file',[fileDir,'\','*.png']);
+    end    
     % In the function 'uiputfile' if the selected file exists in the
     % folder, MATLAB auto-prompts the user with a new GUI window asking
     % for confirmation to overwrite the file.
@@ -24,15 +39,15 @@ else % Figure windows are open
     % This function auto-checks for an existing file name.
     % It prevents the over-writing of existing image files by adding
     % incremental numbers as suffixes to the saved file name.
-    tic
+
     if fileIdx >=1 && fileIdx <= 4
         % Continue execution as the user inputted or selected a file name
         [~,fileName,fileType] = fileparts(fileNameType);
         
         % Check if file(s) with the same name and type exist(s) in the folder
-        if isfile(fileNameType) || exist(fileNameType,'file') == 2
+        if isfile(fileNameType) || exist([fileDir,fileNameType],'file') == 2
             % Find the number of files with the same name and type in the folder
-            fileStruc = dir([fileName,'*',fileType]);
+            fileStruc = dir([fileDir,fileName,'*',fileType]);
             fileNameList = {fileStruc.name};
             numFilesInFolder = sum(contains(fileNameList,fileName));
             disp([num2str(numFilesInFolder),' file(s) of this name & type exist(s) in the folder']);
@@ -50,7 +65,7 @@ else % Figure windows are open
             % Case sensitive re-check for the correct image format
             if any(strcmp(imgType,{'-dbmp','-djpeg','-dpng','-dtiff'}))
                 % Save images
-                print(figFileName,imgType,'-r0');
+                print([fileDir,figFileName],imgType,'-r0');
                 disp(['Image saved as: ',figFileName]);
             else
                 warning('Choose *.bmp, *.jpeg, *.png or *.tiff image types only');
@@ -68,7 +83,7 @@ else % Figure windows are open
         warning('Image save terminated: Execution aborted by user');
         return
     end
-    toc
+
 end
 end
 %---
