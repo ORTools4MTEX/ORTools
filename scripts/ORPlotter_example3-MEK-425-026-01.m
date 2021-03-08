d38ddd2dd7506961d7f4dc94513c1c1ca47976bb
frannie <frannie@mek.dtu.dk>
2021-03-08 15:15:57 +0100

    minor changes

---
+++ scripts/ORPlotter_example3-MEK-425-026-01.m
@@ -0,0 +1,12 @@
+
+% *********************************************************************
+%                        ORPlotter - Example 3
+% *********************************************************************
+% Using the parent-child misorientation peak fitting GUI to deconvolute multiple OR's 
+% *********************************************************************
+% Dr. Azdiar Gazder, 2020, azdiaratuowdotedudotau
+% Dr. Frank Niessen, 2020, contactatfniessendotcom
+% (Remove "dot" and "at" to make this email address valid)
+% License provided in root directory
+% If old style color seleUI needed, type the following and re-start Matlab
+% setpref('Mathworks_uisetcolor', 'Version', 1);
