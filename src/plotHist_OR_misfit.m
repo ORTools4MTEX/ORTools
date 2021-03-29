e2ec0245d95e0a549675fcaca8ea123385c1c7ac
frankNiessen <contact@fniessen.com>
2021-03-29 11:26:52 +0200

    Added binning option

--- src/plotHist_OR_misfit.m
+++ src/plotHist_OR_misfit.m
@@ -10,8 +10,12 @@ function plotHist_OR_misfit(job,varargin)
 %  job          - @parentGrainreconstructor
 %  p2c          - One or multiple orientation-relationships to evaluate
 %
+% Options
+%  nrBins       - Number of bins of the histogram (500 by default)
 
 p2c = getClass(varargin,'orientation');
+nrBins = get_option(varargin,'nrBins',150);
+
 if ~isempty(p2c) && (p2c.CS ~= job.csParent || p2c.SS ~= job.csChild)
     p2c = []; 
 end
@@ -43,9 +47,9 @@ for ii = 1:length(p2c)
     c2c = p2c(ii) * inv(p2c(ii).variants);      
     omegaTemp = angle_outer(moriSub, c2c);
     [omega(ii,:),~] = min(omegaTemp,[],2); 
-    [counts(ii,:),binCenters(ii,:)] = hist(omega(ii,:),500);
+    [counts(ii,:),binCenters(ii,:)] = hist(omega(ii,:),nrBins);
     %--- Normalise the absolute counts in each class interval
-    counts_Norm(ii,:) = 1.*(counts_Norm(ii,:)/sum(counts_Norm(ii,:)));
+    counts_Norm(ii,:) = 1.*(counts(ii,:)/sum(counts(ii,:)));
     h(ii) = area(binCenters(ii,:)./degree, counts_Norm(ii,:),...
         'linewidth',2,...
         'edgecolor',c(ii,:), 'facecolor',c(ii,:),...
@@ -55,16 +59,14 @@ end
 hold off
 
 if ~isempty(job.p2c)
-    h(1).DisplayName = "Current OR"; 
+    h(1).DisplayName = "OR 1**"; 
 end
 
-xlabel('Child-child boundary disorientation (°)','FontSize',14);
+xlabel('Child-child grain disorientation (°)','FontSize',14);
 ylabel('Relative frequency ({\it f}(g))', 'FontSize',14);
-set(gca, 'xlim',[0 max(max(binCenters)./degree)+5]);
-set(gca, 'ylim',[0 max(max(counts))+0.1]);
 grid on
 legend
-set(f,'Name','Child-child disorientation distribution histogram','NumberTitle','on');
+set(f,'Name','Child-child grain disorientation distribution histogram','NumberTitle','on');
 end
     
 
