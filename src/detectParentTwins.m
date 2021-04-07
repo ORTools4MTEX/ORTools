ec2e1222611086eee797e4115cf5abd06f6e9e9d
frankNiessen <contact@fniessen.com>
2021-04-07 16:21:04 +0200

    Refined local twin orientation reconstruction

--- src/detectParentTwins.m
+++ src/detectParentTwins.m
@@ -13,27 +13,39 @@ function detectParentTwins(job,parentEBSD,pGrainId,varargin)
 %  grains       - plot grain data instead of EBSD data
 
 
-%% Define the parent grain
+%% Check if any twins are around
 pGrain = job.parentGrains(job.parentGrains.id2ind(pGrainId));   
 twIds = checkNeighbors(pGrain,job.parentGrains);
 if isempty(twIds)
     fprintf("-> No twins in the parent phase detected\n")
-else
-    twGrains = job.parentGrains(job.parentGrains.id2ind(twIds));
-    twEBSDp = parentEBSD(twGrains);
-    twEBSDc = job.ebsd(twGrains);
-    twgB = job.parentGrains.boundary.selectByGrainId(twIds);
+    return
+end
 
-    %% Check fit of local child orienations to parent and twin orientation
-    newPoris = orientation.nan(size(twEBSDc.orientations),pGrain.CS);
-    for ii = 1:size(twIds,1)
-       for gNr = 1:2 %Loop grain pair
-           %Get parent variant IDs and Fit
-           [pVarIds(:,gNr),fit(:,gNr)] = calcParent(twEBSDc.orientations,twGrains.meanOrientation(gNr,ii),job.p2c,'id');
-           %Get local parent orientations
-           pOris(:,gNr) = variants(job.p2c, twEBSDc.orientations, pVarIds(:,gNr));
-         
-    % *** ATTEMPT OF CHECKING LOCAL FIT - NOT SUCCESFUL        
+%% Prepare IPF color key
+ipfKeyParent = ipfHSVKey(job.csParent);
+ipfKeyParent.inversePoleFigureDirection = getClass(varargin,'vector3d',vector3d.Z);
+
+%% Define the parent grainand find twin related neighbor grains
+twGrains = job.parentGrains(job.parentGrains.id2ind(twIds));
+twEBSDp = parentEBSD(twGrains);
+twEBSDc = job.ebsd(twGrains);
+twgB = job.parentGrains.boundary.selectByGrainId(twIds);
+
+%% Check fit of local child orienations to mean parent and twin orientation
+% and determine the local
+
+for ii = 1:size(twIds,1)
+   for gNr = 1:2 %Loop grain pair
+       %Get parent variant IDs and Fit
+       if all(size(twGrains) == [2,1]) % THIS SHOULD BE FIXED IN MTEX!!
+           [pVarIds(:,gNr),fit(:,ii,gNr)] = calcParent(twEBSDc.orientations,twGrains.meanOrientation(gNr,ii),job.p2c,'id');
+       else
+           [pVarIds(:,gNr),fit(:,ii,gNr)] = calcParent(twEBSDc.orientations,twGrains.meanOrientation(ii,gNr),job.p2c,'id');
+       end
+       %Get local parent orientations
+       pOris(:,ii,gNr) = variants(job.p2c, twEBSDc.orientations, pVarIds(:,gNr));
+
+% *** ATTEMPT OF CHECKING LOCAL FIT - NOT SUCCESFUL        
 %            %Get parent variant IDs
 %            [pVarIds(:,gNr),~] = calcParent(twEBSDc.orientations,twGrains.meanOrientation(gNr,ii),job.p2c,'id');
 %            %Get local parent orientations
@@ -43,32 +55,33 @@ else
 %            pOris(badFit,gNr) = orientation.nan(pGrain.CS);
 %            %Get fit of each orientation
 %            [~,fit(:,gNr)] = calcParent(twEBSDc.orientations,pOris(:,gNr),job.p2c);
-       end   
-
-            %Assign parent orienetations based on best fit
-            fit = fit/degree;
-            [~,id] = min(fit,[],2);
-            newPoris(id == 1) = pOris(id == 1,1);
-            newPoris(id == 2) = pOris(id == 2,2);
-       
-    end
-
-    %% Plot results
-    figure;
-    mtexFig = newMtexFigure('layout',[1,2]);
-    plot(twEBSDp,twEBSDp.orientations);
-    hold on
-    plot(job.grains(twIds).boundary,'linewidth',3);
-    plot(twgB,'lineColor','cyan','linewidth',5,'DisplayName','CSL 3');
-
-    nextAxis
-    for ii = 1:size(twIds,1)
-        plot(twEBSDp,newPoris);
-        hold on
-    end
-    set(gcf,'name','Reconstructed vs. Refined parent orientations');
-
+   end         
 end
+
+    %Assign parent orienetations based on best fit
+fit = fit/degree;
+[~,id] = min(fit,[],[2 3],'linear');    
+newPoris = pOris(ind2sub(size(fit),id));
+
+%% Plot results
+figure;
+%Initial parent EBSD data and twin boundary
+mtexFig = newMtexFigure('layout',[1,2]);
+cbsParent = ipfKeyParent.orientation2color(twEBSDp.orientations);
+plot(twEBSDp,cbsParent);
+hold on
+plot(job.grains(twIds).boundary,'linewidth',3);
+plot(twgB,'lineColor','cyan','linewidth',5,'DisplayName','CSL 3');
+
+%And the refined twin orientations
+nextAxis
+for ii = 1:size(twIds,1)
+    cbsParent = ipfKeyParent.orientation2color(newPoris);
+    plot(twEBSDp,cbsParent);
+    hold on
+end
+set(gcf,'name','Reconstructed vs. Refined parent orientations');
+
 end
 
 function twIds = checkNeighbors(pGrain,grains,varargin)
