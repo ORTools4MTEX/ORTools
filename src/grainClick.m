074b92a0dda236fcc889b3e210b0fe9efaef96d5
frankNiessen <contact@fniessen.com>
2021-04-08 07:35:06 +0200

    bugfix

--- src/grainClick.m
+++ src/grainClick.m
@@ -104,7 +104,7 @@ setappdata(mP.ax,'grains',[pGrains]);
             disp(txt{k});
         end
         % Plot the user-defined stack of plots
-        if any(strcmp(varargin{:},'parentTwins'))
+        if ~isempty(varargin) && any(strcmp(varargin{:},'parentTwins'))
            detectParentTwins(job,parentEBSD,unique(pGrain_select.id),varargin{:});        
         elseif any(job.isTransformed(job.mergeId == pGrain_select.id)) 
             plotStack(job,parentEBSD,unique(pGrain_select.id),varargin{:});
