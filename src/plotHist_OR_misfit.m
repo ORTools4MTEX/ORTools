6b620be8f95a07a13473f606b725b3373c422be3
Azdiar Gazder <azdiar.gazder@gmail.com>
2021-05-05 12:35:00 +1000

    Update plotHist_OR_misfit.m

--- src/plotHist_OR_misfit.m
+++ src/plotHist_OR_misfit.m
@@ -1,6 +1,7 @@
 function plotHist_OR_misfit(job,varargin)
-% Plot histogram of misfit between grain misorientations and the OR
-% misorientation
+% Plot disorientation (or misfit) histogram between 
+% parent-child and child-child grain misorientations 
+% with the OR misorientation
 %
 % Syntax
 %  plotHist_OR_misfit(job)
@@ -30,11 +31,33 @@ if isempty(p2c)
     return;
 end
 
-%% Plot histogram
-%Prepare the boundary misorientations
-c2cPairs = job.grains(job.csChild).neighbors;
-oriChild = reshape(job.grains('id',c2cPairs).meanOrientation,[],2);
-mori = inv(oriChild(:,1)).*oriChild(:,2);
+%% Plot p2c pairs histogram
+p2cPairs = neighbors(job.grains(job.csParent),job.grains(job.csChild));
+p2cPairs = p2cPairs(:,flip([1 2]));
+if ~isempty(p2cPairs)
+    misfitHist(p2c,job,p2cPairs,numBins,allLegend,'pairType','p2c')
+else
+    warning('No parent-child neighbors found (p2c grain pairs empty)');
+end
+
+%% Plot c2c pairs histogram
+c2cPairs = neighbors(job.grains(job.csChild),job.grains(job.csChild));
+c2cPairs = c2cPairs(:,flip([1 2]));
+if ~isempty(c2cPairs)
+    misfitHist(p2c,job,c2cPairs,numBins,allLegend,'pairType','c2c')
+else
+    warning('No child-child neighbors found (c2c grain pairs empty)');
+end
+
+end
+
+
+
+function misfitHist(p2c,job,pairList,numBins,allLegend,varargin)
+pairType = get_option(varargin,'pairType','');
+
+mori = inv(job.grains('id',pairList(:,1)).meanOrientation).*...
+    job.grains('id',pairList(:,2)).meanOrientation;
 mori(mori.angle < 5*degree) = [];
 if length(mori) > 50000
     moriSub = discreteSample(mori,50000);
@@ -42,12 +65,19 @@ else
     moriSub = mori;
 end
 
-%Plot the c2c grain disorientation
+% Plot the grain pair disorientation
 f = figure;
 c = ind2color(1:length(p2c));
 for ii = 1:length(p2c)
-    c2c = p2c(ii) * inv(p2c(ii).variants);
-    omegaTemp = angle_outer(moriSub,c2c);
+    if strcmp(pairType,'p2c')==1
+        omegaTemp = angle_outer(moriSub,p2c(ii));
+    elseif strcmp(pairType,'c2c')==1
+        c2c = p2c(ii) * inv(p2c(ii).variants);
+        omegaTemp = angle_outer(moriSub,c2c);
+    elseif strcmp(pairType,'')==1
+        warning('Grain pair type not specified)');
+        return
+    end
     [omega(ii,:),~] = min(omegaTemp,[],2);
     %--- Calculate the counts in each class interval
     [counts(ii,:),binCenters(ii,:)] = hist(omega(ii,:),numBins);
@@ -73,11 +103,14 @@ if ~isempty(allLegend)
     end
 end
 
-xlabel('Child-child grain disorientation (°)','FontSize',14);
-ylabel('Relative frequency ({\itf}(g))', 'FontSize',14);
 grid on
 legend
-set(f,'Name','Child-child grain disorientation distribution histogram','NumberTitle','on');
+ylabel('Relative frequency ({\itf}(g))', 'FontSize',14);
+if strcmp(pairType,'p2c')==1
+    xlabel('Parent-child grain disorientation (°)','FontSize',14);
+    set(f,'Name','Parent-child grain disorientation distribution histogram','NumberTitle','on');
+elseif strcmp(pairType,'c2c')==1
+    xlabel('Child-child grain disorientation (°)','FontSize',14);
+    set(f,'Name','Child-child grain disorientation distribution histogram','NumberTitle','on');
 end
-
-
+end
