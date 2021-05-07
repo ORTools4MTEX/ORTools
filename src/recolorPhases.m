9c8246a29d9c91d22796fa6b1a7dea80bfd91744
Azdiar Gazder <azdiar.gazder@gmail.com>
2021-05-07 10:19:23 +1000

    Update recolorPhases.m

--- src/recolorPhases.m
+++ src/recolorPhases.m
@@ -1,9 +1,9 @@
-function [ebsd] = recolorPhases(ebsd)
+function ebsd = recolorPhases(ebsd)
 % recolor phases in ebsd and grains interactively
 %
 % Syntax
 %
-%  [ebsd] = recolorPhases(ebsd)
+%  ebsd = recolorPhases(ebsd)
 %
 % Input
 %  ebsd             - @EBSD
@@ -14,32 +14,26 @@ function [ebsd] = recolorPhases(ebsd)
 % If old style color seleUI needed, type the following and re-start Matlab
 % setpref('Mathworks_uisetcolor', 'Version', 1);
 
-%% ReColor the phases (minerals)
+%% Recolor the phases (minerals)
 fprintf(' -> Define the phase color\n');
-phaseNum = length(ebsd.CSList);
-phaseNames = char(ebsd.mineralList(1:numel(ebsd.mineralList)));
-%% Auto-generate grayscale colors
-scale = linspace(0.3,0.95,phaseNum+1)';
-ebsd.opt.cGS = repmat(scale(1:end-1),1,3);
+numPhases = length(ebsd.CSList);
+phaseName = char(ebsd.mineralList(1:numel(ebsd.mineralList)));
 %% Selecting colors
 fprintf(' -> Recoloring all phases\n');
-cRGB = ones(phaseNum,3);
-for ii = 2:phaseNum    
-    fprintf('    - ''%s''\n',phaseNames(ii,:))
-    promptString = ['Define RGB for ''',phaseNames(ii,:),''''];
+for ii = 2:numPhases
+    fprintf('    - ''%s''\n',phaseName(ii,:))
+    promptString = ['Define RGB for ''',phaseName(ii,:),''''];
     try
-        tempRGB = uisetcolor([],promptString);
+        cRGB = uisetcolor([],promptString);
         
-        if tempRGB == 0 %%&& size(tempRGB,2) == 1
-            % Response to "Cancel" or "Close" buttons
-            warning('Phase recoloring aborted by user: Keeping default colors');
-                return
-        else
-            %% Add phase colors
-            cRGB(ii,:) = tempRGB;
-            clear tempRGB
-            ebsd.CSList{ii}.color = cRGB(ii,:);            
+        if cRGB == 0 % Response to "Cancel" or "Close" buttons
+            warning('Phase recoloring aborted by user: Keeping previous colors');
+            return
+        else % Recolor phase
+            ebsd.CSList{ii}.color = cRGB;
+            clear cRGB
         end
+        
     catch
     end
 end
