2860b68c2d7414fe558704fa5aef425e3c362e70
Azdiar Gazder <azdiar.gazder@gmail.com>
2021-05-12 17:24:12 +1000

    Update recolorPhases.m

--- src/recolorPhases.m
+++ src/recolorPhases.m
@@ -1,23 +1,27 @@
-function ebsd = recolorPhases(ebsd)
-% recolor phases in ebsd and grains interactively
+function obj = recolorPhases(obj)
+% recolor phases in ebsd or grains interactively
 %
 % Syntax
 %
-%  ebsd = recolorPhases(ebsd)
+%  ebsd   = recolorPhases(ebsd)
+%  grains = recolorGrains(grains)
 %
 % Input
 %  ebsd             - @EBSD
+%  grains           - @grains
 %
 % Output
 %  ebsd             - @EBSD
+%  grains           - @grains
 
 % If old style color seleUI needed, type the following and re-start Matlab
 % setpref('Mathworks_uisetcolor', 'Version', 1);
 
 %% Recolor the phases (minerals)
+if isa(obj,'EBSD') || isa(obj,'grain2d')
 fprintf(' -> Define the phase color\n');
-numPhases = length(ebsd.CSList);
-phaseName = char(ebsd.mineralList(1:numel(ebsd.mineralList)));
+numPhases = length(obj.CSList);
+phaseName = char(obj.mineralList(1:numel(obj.mineralList)));
 %% Selecting colors
 fprintf(' -> Recoloring all phases\n');
 for ii = 2:numPhases
@@ -30,15 +34,15 @@ for ii = 2:numPhases
             warning('Phase recoloring aborted by user: Keeping previous colors');
             return
         else % Recolor phase
-            ebsd.CSList{ii}.color = cRGB;
+            obj.CSList{ii}.color = cRGB;
             clear cRGB
         end
         
     catch
     end
 end
+else
+    msg = 'Incorrect variable type: Only "ebsd" or "grains" accepted.';
+    error(msg);
 end
-
-
-
-
+end
