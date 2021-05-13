5d23ffc7aa51539dfae1dcc210d57c3271a70f0e
Azdiar Gazder <azdiar.gazder@gmail.com>
2021-05-13 12:39:45 +1000

    Update ORTools_example6.m

--- ORTools_example6.m
+++ ORTools_example6.m
@@ -2,7 +2,7 @@
 % *********************************************************************
 %                        ORTools - Example 6
 % *********************************************************************
-% Conducting a two-stage parent grain reconstruction in a TRWIP steel
+% Parent grain reconstruction in two-stages in a TWIP-TRIP steel
 % *********************************************************************
 % Dr. Azdiar Gazder, 2021, azdiaratuowdotedudotau
 % Dr. Frank Niessen, 2021, contactatfniessendotcom
@@ -23,7 +23,7 @@ Ini.cifPath = [Ini.dataPath,'input/cif/'];
 Ini.ebsdPath = [Ini.dataPath,'input/ebsd/'];
 Ini.texturePath = [Ini.dataPath,'output/texture/'];
 Ini.imagePath = [Ini.dataPath,'output/images/'];
-Ini.phaseNames = {'Gamma','Epsilon','AlphaP'};
+Ini.phaseNames = {'Gamma','AlphaP','Epsilon'};
 %% Import EBSD data and save current file name
 ebsd = loadEBSD_ctf([Ini.ebsdPath,'TRWIPsteel2.ctf'],'convertSpatial2EulerReferenceFrame');
 ebsd = ebsd('indexed');
@@ -48,36 +48,36 @@ plot(grains.boundary,'linewidth',2);
 %% Define the transformation system
 screenPrint('SegmentStart','Finding the orientation relationship(s)');
 % Choose "Epsilon" as a parent and "AlphaP" as a child phase
-job = setParentGrainReconstructor(ebsd,grains,Ini.cifPath);
+job1 = setParentGrainReconstructor(ebsd,grains,Ini.cifPath);
 %% Plot parent and child IPF maps
-plotMap_IPF_p2c(job,vector3d.Z,'linewidth',2);
+plotMap_IPF_p2c(job1,vector3d.Z,'linewidth',2);
 
 % ************** %
 %% Define and check the OR EPSILON - ALPHA
 % We define the Burgers OR between Epsilon and AlphaP
-job.p2c = orientation.Burgers(job.csParent,job.csChild);
-plotHist_OR_misfit(job);
-job.calcParent2Child('p2c');
-plotHist_OR_misfit(job);
+job1.p2c = orientation.Burgers(job1.csParent,job1.csChild);
+plotHist_OR_misfit(job1);
+job1.calcParent2Child('p2c');
+plotHist_OR_misfit(job1);
 %% Check the fit with the OR locally
 % Plot parent-child and child-child OR boundary disorientation map
 % We color the boundaries up to 5° disorientation to emphasize the effects
-plotMap_gB_misfit(job,'linewidth',1.5,'maxColor',5);
+plotMap_gB_misfit(job1,'linewidth',1.5,'maxColor',5);
 % The fit is quite good most places
 %% Reconstruct alphaP
 for k = 1:3
-  job.calcGBVotes('p2c','threshold',k*2.5*degree);
-  job.calcParentFromVote;
+  job1.calcGBVotes('p2c','threshold',k*2.5*degree);
+  job1.calcParentFromVote;
 end
-plotMap_IPF_p2c(job,vector3d.Z,'linewidth',2,'parent');
+plotMap_IPF_p2c(job1,vector3d.Z,'linewidth',2,'parent');
 %% Merge similar
-job.mergeSimilar('threshold',7.5*degree);
-plotMap_IPF_p2c(job,vector3d.Z,'linewidth',2,'parent');
+job1.mergeSimilar('threshold',7.5*degree);
+plotMap_IPF_p2c(job1,vector3d.Z,'linewidth',2,'parent');
 
 % ************** %
 %% Define and check the OR GAMMA - EPSILON
 % Choose "Gamma" as a parent and "Epsilon" as a child phase
-job2 = setParentGrainReconstructor(job.ebsd,job.grains,Ini.cifPath);
+job2 = setParentGrainReconstructor(job1.ebsd,job1.grains,Ini.cifPath);
 % We define the Shoji Nishiyama OR between Gamma and Epsilon
 job2.p2c = orientation.ShojiNishiyama(job2.csParent,job2.csChild);
 plotHist_OR_misfit(job2);
@@ -98,6 +98,7 @@ plotMap_IPF_p2c(job2,vector3d.Z,'linewidth',2,'parent');
 job2.mergeSimilar('threshold',7.5*degree);
 plotMap_IPF_p2c(job2,vector3d.Z,'linewidth',2,'parent');
 
+% ************** %
 %% Variant analysis Gamma-Epsilon
 plotPDF_variants(job2);
 % Then calculate the variant IDs of all alpha grains ...
@@ -105,12 +106,13 @@ job2.calcVariants;
 % ... and plot them
 plotMap_variants(job2,'linewidth',3);
 
+% ************** %
 %% Variant analysis Epsilon-AlphaP
-plotPDF_variants(job);
+plotPDF_variants(job1);
 % Then calculate the variant IDs of all alpha grains ...
-job.calcVariants;
+job1.calcVariants;
 % ... and plot them
-plotMap_variants(job,'linewidth',3);
+plotMap_variants(job1,'linewidth',3);
 
 %% Save images
 saveImage(Ini.imagePath);
@@ -118,8 +120,8 @@ saveImage(Ini.imagePath);
 %% Grain Click
 grainClick(job2,'noScalebar','noFrame');
 %% Summary
-% This example has shown how to identify different ORs and revert them
-% individually. In the present case, we found an OR corresponding to a
-% cube-on-cube orientation relationship, which was caused by misindexing
-% gamma as alpha. We reverted these grains to gamma in order to clean the
-% map. 
+% This example has shown how to conduct parent grain reconstruction in 
+% two-stages in a TWIP-TRIP steel. In the first stage, alphaP martensite 
+% was reverted to epsilon martensite. In the second stage, epsilon 
+% martensite was reverted to parent austenite.
+
