fb75c98c0682b5a978f6cd7088789e1c89098c48
Azdiar Gazder <azdiar.gazder@gmail.com>
2021-06-03 09:59:09 +1000

    Update plotMap_packets.m

--- src/plotMap_packets.m
+++ src/plotMap_packets.m
@@ -24,9 +24,9 @@ else
     isParent = pGrains.phaseId == job.parentPhaseId;
     pGrains = pGrains(isParent);
     cEBSD = job.ebsdPrior(job.csChild);
-    cEBSD = cEBSD(isParent); 
+    cEBSD = cEBSD(isParent);
     [~,packIds] = calcVariantId(pGrains.meanOrientation,cEBSD.orientations,job.p2c,'variantMap',job.variantMap,varargin{:});
-    plot(cEBSD,packIds);   
+    plot(cEBSD,packIds);
 end
 
 
@@ -36,13 +36,13 @@ plot(parentGrains.boundary,varargin{:})
 hold off
 
 % Define the maximum number of color levels and plot the colorbar
-    maxColors = max(job.transformedGrains.packetId);
-    colormap(cmap);
-    caxis([1 maxColors]);
-    colorbar('location','eastOutSide','LineWidth',1.25,'TickLength', 0.01,...
-        'YTick', [1:1:maxColors],...
-        'YTickLabel',num2str([1:1:maxColors]'), 'YLim', [1 maxColors],...
-        'TickLabelInterpreter','latex','FontName','Helvetica','FontSize',14,'FontWeight','bold');
-    set(f,'Name','Packet Id map','NumberTitle','on');
-    drawnow;
+maxColors = max(job.transformedGrains.packetId);
+colormap(cmap);
+caxis([1 maxColors]);
+colorbar('location','eastOutSide','LineWidth',1.25,'TickLength', 0.01,...
+    'YTick', [1:1:maxColors],...
+    'YTickLabel',num2str([1:1:maxColors]'), 'YLim', [1 maxColors],...
+    'TickLabelInterpreter','latex','FontName','Helvetica','FontSize',14,'FontWeight','bold');
+set(f,'Name','Packet Id map','NumberTitle','on');
+drawnow;
 end
