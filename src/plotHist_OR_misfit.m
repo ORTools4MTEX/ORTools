6772e55ba748a96014c4cfbdd87774856080134e
Azdiar Gazder <azdiar.gazder@gmail.com>
2021-06-02 14:27:55 +1000

    Update plotHist_OR_misfit.m

--- src/plotHist_OR_misfit.m
+++ src/plotHist_OR_misfit.m
@@ -15,6 +15,9 @@ function plotHist_OR_misfit(job,varargin)
 %  bins         - number of histogram bins
 %  legend       - cell array of strings with legend names of ORs to evaluate
 
+%% Define the default window settings for figures
+set(0,'DefaultFigureWindowStyle','normal');
+
 p2c = getClass(varargin,'orientation');
 numBins = get_option(varargin,'bins',150);
 allLegend = get_option(varargin,'legend',{});
@@ -31,11 +34,26 @@ if isempty(p2c)
     return;
 end
 
+%% Define the window settings for a set of docked figures
+desktop = com.mathworks.mde.desk.MLDesktop.getInstance;
+% Define a unique group name for the dock
+groupName = ['plotHist',num2str(randi(100))];
+figGroup = desktop.addGroup(groupName);
+desktop.setGroupDocked(groupName,0);
+figDim = java.awt.Dimension(2,1);  % 2 columns, 1 rows
+% % Define how the figure appear in the dock 
+% % 1 = Maximized, 2 = Tiled, 3 = Floating
+desktop.setDocumentArrangement(groupName,1,figDim)
+% Define the graphics object array for the number of figures to be plotted
+figH = gobjects(2,1);
+bakWarn = warning('off','MATLAB:HandleGraphics:ObsoletedProperty:JavaFrame');
+
+
 %% Plot p2c pairs histogram
 p2cPairs = neighbors(job.grains(job.csParent),job.grains(job.csChild));
 p2cPairs = p2cPairs(:,flip([1 2]));
 if ~isempty(p2cPairs)
-    misfitHist(p2c,job,p2cPairs,numBins,allLegend,'pairType','p2c')
+    misfitHist(groupName,figH,p2c,job,p2cPairs,numBins,allLegend,'pairType','p2c')
 else
     warning('No parent-child neighbors found (p2c grain pairs empty)');
 end
@@ -44,16 +62,30 @@ end
 c2cPairs = neighbors(job.grains(job.csChild),job.grains(job.csChild));
 c2cPairs = c2cPairs(:,flip([1 2]));
 if ~isempty(c2cPairs)
-    misfitHist(p2c,job,c2cPairs,numBins,allLegend,'pairType','c2c')
+    misfitHist(groupName,figH,p2c,job,c2cPairs,numBins,allLegend,'pairType','c2c')
 else
     warning('No child-child neighbors found (c2c grain pairs empty)');
 end
 
+%% Place first tabbed figure on top, undock the figure window and return
+allfigh = findall(0,'type','figure');
+if length(allfigh) > 1
+    figure(length(allfigh)-1);
+end
+set(0,'DefaultFigureWindowStyle','normal');
+warning(bakWarn);
+clear misfitHist
+return
 end
 
 
 
-function misfitHist(p2c,job,pairList,numBins,allLegend,varargin)
+function misfitHist(groupName,figH,p2c,job,pairList,numBins,allLegend,varargin)
+% Setup a counter for the number of times the plotting function was called
+persistent count;
+if isempty(count); count = 0; end
+count = count+1
+
 pairType = get_option(varargin,'pairType','');
 
 mori = inv(job.grains('id',pairList(:,1)).meanOrientation).*...
@@ -66,7 +98,9 @@ else
 end
 
 % Plot the grain pair disorientation
-f = figure;
+figH(count) = figure('WindowStyle','docked');
+set(get(handle(figH(count)),'javaframe'),'GroupName',groupName);
+pause(0.05);  % Reduce rendering errors
 c = ind2color(1:length(p2c));
 for ii = 1:length(p2c)
     if strcmp(pairType,'p2c')==1
@@ -108,9 +142,10 @@ legend
 ylabel('Relative frequency ({\itf}(g))','FontSize',14);
 if strcmp(pairType,'p2c')==1
     xlabel('Parent-child grain disorientation (°)','FontSize',14);
-    set(f,'Name','Parent-child grain disorientation histogram','NumberTitle','on');
+    set(figH(count),'Name','Parent-child grain disorientation histogram','NumberTitle','on');
 elseif strcmp(pairType,'c2c')==1
     xlabel('Child-child grain disorientation (°)','FontSize',14);
-    set(f,'Name','Child-child grain disorientation histogram','NumberTitle','on');
+    set(figH(count),'Name','Child-child grain disorientation histogram','NumberTitle','on');
 end
+pause(0.05);  % Reduce rendering errors
 end
