64948cd41cbc3bdeed6bdeaa51587000d3a64d9c
Azdiar Gazder <azdiar.gazder@gmail.com>
2021-06-02 15:29:27 +1000

    Update plotHist_OR_misfit.m

--- src/plotHist_OR_misfit.m
+++ src/plotHist_OR_misfit.m
@@ -15,9 +15,6 @@ function plotHist_OR_misfit(job,varargin)
 %  bins         - number of histogram bins
 %  legend       - cell array of strings with legend names of ORs to evaluate
 
-%% Define the default window settings for figures
-set(0,'DefaultFigureWindowStyle','normal');
-
 p2c = getClass(varargin,'orientation');
 numBins = get_option(varargin,'bins',150);
 allLegend = get_option(varargin,'legend',{});
@@ -35,12 +32,13 @@ if isempty(p2c)
 end
 
 %% Define the window settings for a set of docked figures
+% % Ref: https://au.mathworks.com/matlabcentral/answers/157355-grouping-figures-separately-into-windows-and-tabs
 desktop = com.mathworks.mde.desk.MLDesktop.getInstance;
 % Define a unique group name for the dock
 groupName = ['plotHist',num2str(randi(100))];
-figGroup = desktop.addGroup(groupName);
+% figGroup = desktop.addGroup(groupName);
 desktop.setGroupDocked(groupName,0);
-figDim = java.awt.Dimension(2,1);  % 2 columns, 1 rows
+figDim = java.awt.Dimension(2,1);  % 2 columns, 1 row
 % % Define how the figure appear in the dock 
 % % 1 = Maximized, 2 = Tiled, 3 = Floating
 desktop.setDocumentArrangement(groupName,1,figDim)
@@ -67,24 +65,23 @@ else
     warning('No child-child neighbors found (c2c grain pairs empty)');
 end
 
-%% Place first tabbed figure on top, undock the figure window and return
+%% Place first tabbed figure on top, clear the counter and return
 allfigh = findall(0,'type','figure');
 if length(allfigh) > 1
     figure(length(allfigh)-1);
 end
-set(0,'DefaultFigureWindowStyle','normal');
 warning(bakWarn);
-clear misfitHist
+clear cnt misfitHist
 return
 end
 
 
 
 function misfitHist(groupName,figH,p2c,job,pairList,numBins,allLegend,varargin)
-% Setup a counter for the number of times the plotting function was called
-persistent count;
-if isempty(count); count = 0; end
-count = count+1;
+% Setup a counter for the number of times this plotting function is called
+persistent cnt;
+if isempty(cnt); cnt = 0; end
+cnt = cnt+1
 
 pairType = get_option(varargin,'pairType','');
 
@@ -98,9 +95,10 @@ else
 end
 
 % Plot the grain pair disorientation
-figH(count) = figure('WindowStyle','docked');
-set(get(handle(figH(count)),'javaframe'),'GroupName',groupName);
+figH(cnt) = figure('WindowStyle','docked')
+set(get(handle(figH(cnt)),'javaframe'),'GroupName',groupName);
 pause(0.05);  % Reduce rendering errors
+
 c = ind2color(1:length(p2c));
 for ii = 1:length(p2c)
     if strcmp(pairType,'p2c')==1
@@ -142,10 +140,10 @@ legend
 ylabel('Relative frequency ({\itf}(g))','FontSize',14);
 if strcmp(pairType,'p2c')==1
     xlabel('Parent-child grain disorientation (°)','FontSize',14);
-    set(figH(count),'Name','Parent-child grain disorientation histogram','NumberTitle','on');
+    set(figH(cnt),'Name','Parent-child grain disorientation histogram','NumberTitle','on');
 elseif strcmp(pairType,'c2c')==1
     xlabel('Child-child grain disorientation (°)','FontSize',14);
-    set(figH(count),'Name','Child-child grain disorientation histogram','NumberTitle','on');
+    set(figH(cnt),'Name','Child-child grain disorientation histogram','NumberTitle','on');
 end
 pause(0.05);  % Reduce rendering errors
 end
