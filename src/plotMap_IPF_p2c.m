79b423e5e1f5e7bd25cd510cb4b5c7a471e26141
Azdiar Gazder <azdiar.gazder@gmail.com>
2021-06-03 15:26:52 +1000

    Update plotMap_IPF_p2c.m

--- src/plotMap_IPF_p2c.m
+++ src/plotMap_IPF_p2c.m
@@ -1,5 +1,5 @@
 function ipfKey = plotMap_IPF_p2c(job, varargin)
-% plot inverse polefigure maps of parent and child phases
+% plot inverse pole figure maps of parent and child phases
 %
 % Syntax
 %  plotMap_IPF_p2c(job)
@@ -21,20 +21,35 @@ ipfKey1 = [];
 ipfKey2 = [];
 
 
-% Check what to plot
-onlyChild = false; 
-onlyParent = false; 
+%% Check what to plot
+onlyChild = false;
+onlyParent = false;
 if check_option(varargin,'child')
-   onlyChild = true; 
-   varargin(find_option(varargin,'child'))=[];
+    onlyChild = true;
+    varargin(find_option(varargin,'child'))=[];
 elseif check_option(varargin,'parent')
-   onlyParent = true; 
-   varargin(find_option(varargin,'parent'))=[];
+    onlyParent = true;
+    varargin(find_option(varargin,'parent'))=[];
 end
 
-% Parent map
+%% Define the window settings for a set of docked figures
+% % Ref: https://au.mathworks.com/matlabcentral/answers/157355-grouping-figures-separately-into-windows-and-tabs
+warning off
+desktop = com.mathworks.mde.desk.MLDesktop.getInstance;
+% % Define a unique group name for the dock using the function name
+% % and the system timestamp
+dockGroupName = ['plotPODF_transform_',char(datetime('now','Format','yyyyMMdd_HHmmSS'))];
+desktop.setGroupDocked(dockGroupName,0);
+bakWarn = warning('off','MATLAB:HandleGraphics:ObsoletedProperty:JavaFrame');
+
+
+
+%% Parent map
 if ~isempty(job.ebsd(job.csParent)) && ~onlyChild
-    f = figure;
+    figH = gobjects(1);
+    figH = figure('WindowStyle','docked');
+    set(get(handle(figH),'javaframe'),'GroupName',dockGroupName);
+    drawnow;
     ipfKey1 = ipfHSVKey(job.ebsd(job.csParent));
     ipfKey1.inversePoleFigureDirection = vector;
     colors = ipfKey1.orientation2color(job.ebsd(job.csParent).orientations);
@@ -43,7 +58,7 @@ if ~isempty(job.ebsd(job.csParent)) && ~onlyChild
     plot(job.grains.boundary,varargin{:});
     hold off
     guiTitle = ['Parent IPF <',vector.char,'> map = ',job.csParent.mineral];
-    set(f,'Name',guiTitle,'NumberTitle','on');
+    set(figH,'Name',guiTitle,'NumberTitle','on');
     drawnow;
 elseif isempty(job.ebsd(job.csParent))
     warning('Parent IPFx map empty');
@@ -51,19 +66,38 @@ end
 
 % Child map
 if ~isempty(job.ebsd(job.csChild)) && ~onlyParent
-    f = figure;
+    figH = gobjects(1);
+    figH = figure('WindowStyle','docked');
+    set(get(handle(figH),'javaframe'),'GroupName',dockGroupName);
+    drawnow;
     ipfKey2 = ipfHSVKey(job.ebsd(job.csChild));
     ipfKey2.inversePoleFigureDirection = vector;
     colors = ipfKey2.orientation2color(job.ebsd(job.csChild).orientations);
     plot(job.ebsd(job.csChild),colors);
-    hold all  
+    hold all
     plot(job.grains.boundary,varargin{:});
     hold off
     guiTitle = ['Child IPF <',vector.char,'> map = ',job.csChild.mineral];
-    set(f,'Name',guiTitle,'NumberTitle','on');
+    set(figH,'Name',guiTitle,'NumberTitle','on');
     drawnow;
 elseif isempty(job.ebsd(job.csChild))
     warning('Child IPFx map empty');
 end
 ipfKey = [ipfKey1,ipfKey2];
+
+%% Place first tabbed figure on top and return
+warning on
+allfigh = findall(0,'type','figure');
+
+if length(allfigh) > 1 &&...
+        ~isempty(job.ebsd(job.csParent)) && ~onlyChild &&...
+        ~isempty(job.ebsd(job.csChild)) && ~onlyParent
+    figure(length(allfigh)-1);
+else
+    figure(1);
+end
+warning(bakWarn);
+pause(1); % Reduce rendering errors
+return
+
 end
