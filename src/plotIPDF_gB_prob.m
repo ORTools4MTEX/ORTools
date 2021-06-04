35f52f58a608bbfe8304f31538c02539e9098f9e
Azdiar Gazder <azdiar.gazder@gmail.com>
2021-06-04 12:06:14 +1000

    Update plotIPDF_gB_prob.m

--- src/plotIPDF_gB_prob.m
+++ src/plotIPDF_gB_prob.m
@@ -76,7 +76,7 @@ warning off
 desktop = com.mathworks.mde.desk.MLDesktop.getInstance;
 % % Define a unique group name for the dock using the function name
 % % and the system timestamp
-dockGroupName = ['plotIPDF_gB_misfit_',char(datetime('now','Format','yyyyMMdd_HHmmSS'))];
+dockGroupName = ['plotIPDF_gB_prob_',char(datetime('now','Format','yyyyMMdd_HHmmSS'))];
 desktop.setGroupDocked(dockGroupName,0);
 bakWarn = warning('off','MATLAB:HandleGraphics:ObsoletedProperty:JavaFrame');
 
