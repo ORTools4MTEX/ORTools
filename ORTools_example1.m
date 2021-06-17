2a3564cf877fb8fe0da234c96cb125ea95d3f2fd
Azdiar Gazder <azdiar.gazder@gmail.com>
2021-06-17 11:24:08 +1000

    Update ORTools_example1.m

--- ORTools_example1.m
+++ ORTools_example1.m
@@ -62,7 +62,7 @@ job.calcParent2Child;
 % and the misorientation of the OR)
 KS = orientation.KurdjumovSachs(job.csParent,job.csChild);
 NW = orientation.NishiyamaWassermann(job.csParent,job.csChild);
-plotHist_OR_misfit(job,[KS,NW],'legend',{'K-S','N-W'});
+plotHist_OR_misfit(job,[KS,NW],'legend',{'K-S OR','N-W OR'});
 % Plot information about the OR
 ORinfo(job.p2c);
 %    - There are 24 martensitic variants
