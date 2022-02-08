cf08f9caa813a0600f3b1cd512eb041574928608
Azdiar Gazder <azdiar.gazder@gmail.com>
2022-02-08 11:05:40 +1100

    Create setLabels2Tex.m

---
+++ src/setLabels2Tex.m
@@ -0,0 +1,20 @@
+function setLabels2Tex
+% https://au.mathworks.com/matlabcentral/answers/183311-setting-default-interpreter-to-latex
+% Change all interpreters from 'latex' to 'tex'
+factoryList = fieldnames(get(groot,'factory'));
+interpreterIdx = find(contains(factoryList,'Interpreter'));
+for ii = 1:length(interpreterIdx)
+    defaultName = strrep(factoryList{interpreterIdx(ii)},'factory','default');
+    set(groot,defaultName,'tex');
+end
+end
+
+
+% set(0,'defaultTextInterpreter','latex');
+% set(groot,'defaultAxesTickLabelInterpreter','latex');
+% set(groot,'defaultLegendInterpreter','latex');
+
+% ylabel('Relative block boundary frequency [$f$(g)]');
+% ylabel('\bf Relative block boundary frequency [$\bf f$(g)]');
+% ylabel('Block boundary density [$\mu m / \mu m^{2}$]')
+% ylabel('\bf Block boundary density [$\bf \mu m / \mu m^{2}$]')
