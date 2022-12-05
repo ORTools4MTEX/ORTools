14943e22cb93fffff1e159f6d2c4a6b49e24c094
frannie <frannie@mek.dtu.dk>
2022-12-05 20:04:05 +0100

    Added getPacketGrains - merging child grains according to packet ID

---
+++ src/getPacketGrains.m
@@ -0,0 +1,29 @@
+function packets = getPacketGrains(job, varargin)
+% get the packet grains
+%
+% Syntax
+%  getPacketGrains(job)
+%
+% Input
+%  job          - @parentGrainreconstructor
+%
+% Options
+
+%Get the grain boundaries
+gB = job.grainsPrior.boundary;
+%Only get child to child boundaries
+gB = gB(job.csChild,job.csChild);
+%Get packet Ids
+packetId = job.packetId;
+%Get the packet numbers of the grains at the boundaries
+ids = packetId(gB.grainId);
+%Check where they are identical
+isPacket = ids(:,1) - ids(:,2) == 0;
+%Get the packet boundaries
+packetBoundary = gB(isPacket);
+%Get packets
+[packets,parentId] = merge(job.grainsPrior,packetBoundary);
+ pId = nan(length(packets),1);
+for i=1:length(packets)
+    pId(i) = median(packetId(packets(i).id==parentId),"all","omitnan");
+end
