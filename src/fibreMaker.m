807cb6f72f766d3b11a40c966b11c207daac1a49
azdiarGazder <azdiar.gazder@gmail.com>
2023-04-24 16:44:39 +1000

    Update fibreMaker.m

--- src/fibreMaker.m
+++ src/fibreMaker.m
@@ -1,88 +1,74 @@
-function fibreMaker(crystalDirection,specimenDirection,varargin)
-% create a fibre ODF and export a VPSC file
+function fibreMaker(crystalDirection,sampleDirection,sampleSymmetry,varargin)
+%% Function description:
+% Creates an ideal crystallographic fibre with a user specified
+% half-width and exports the data as a lossless Mtex *.txt file (for 
+% version 5.9.0 and onwards) or as a lossy discretised Mtex *.txt file (for 
+% up to version 5.8.2) for later use.
 %
-% Syntax
-%  fibreMaker(crystalDirection,specimenDirection)
+%% Author:
+% Dr. Azdiar Gazder, 2023, azdiaratuowdotedudotau
 %
-% Input
+%% Modified by:
+% Dr. Frank Niessen to include varargin.
+%
+%% Version(s):
+% The first version of this function was posted in:
+% https://github.com/ORTools4MTEX/ORTools/blob/develop/fibreMaker.m
+%
+%% Syntax:
+%  fibreMaker(crystalDirection,samplenDirection,,sampleSymmetry)
+%
+%% Input:
 %  crystalDirection     - @Miller
-%  specimenDirection    - @vector3d
+%  sampleDirection      - @vector3d
+%  sampleSymmetry       - @specimenSymmetry
 %
-% Options
-%  halfwidth    - halfwidth for ODF calculation
-%  points       - number of points (orientations) in the VPSC file
-%  export       - (optional path) & name of the VPSC file
-
-
-%% All fibres are defined based on the following specimen co-ordinate system
-% RD = xvector; TD = yvector; ND = zvector;
-
-%% Common fibers for fcc materials
-% % alpha-fibre = <110> || ND
-% % beta-fibre = <110> tilted 60° from ND towards TD
-% % gamma-fibre = <111> || ND
-% % tau-fibre = <110> || TD
-% % REF: https://doi.org/10.1016/j.actamat.2011.05.050
-% % theta-fibre = <001> || ND
-
-
-%% Common fibers for bcc materials
-% % Alpha-fibre = <110> || RD
-% % Eta-fibre = <100> || RD
-% % Epsilon-fibre = <110> || TD
-% % Gamma-fibre = <111> || ND
-% % GammaPrime-fibre = <223> || ND
-% % Theta-fibre = <100> || ND
-% % Zeta-fibre = <110> || ND
-% % REF:  https://doi.org/10.1002/adem.201000075
-% % {h 1 1} <1/h 1 2> fibre
-% %  Notes: This fibre can be simplified as a nominal bcc alpha-fibre
-% % whose <110> is tilted 20° from RD towards ND 
-
-
-%% Common fibres for hcp materials
-% % 0001-fibre = <0 0 0 1> || ND
-% % 11-20-fibre = <1 1 -2 0> || RD
-% % 10-10-fibre = <1 0 -1 0> || ND
-% % hkil-fibre = <0 0 0 1> tilted 20° from ND towards RD
-
-
-%% How to define fibres as used in the function
-%% Example 1 
-% % Applied to the bcc {h 1 1} <1/h 1 2> fibre 
-% % which can be simplified as a nominal bcc alpha-fibre 
-% % whose <110> is tilted 20° from RD towards ND 
-%% Define a crystallographic direction
-% cD = Miller({1,1,0},crystalSystem.parent,'uvw');
-%% Define a sample direction parallel to the crystallographic direction
-% % sD = RD;
-%% Define a tilt away a specimen co-ordinate system direction 
-% % rotN = rotation('Euler',-20*degree,0*degree,0*degree);
-% % sD = rotN * RD;
-
-%% Example 2 
-% % Applied to the fcc beta fibre 
-% % whose <110> is tilted 60° from ND towards TD 
-%% Define a crystallographic direction
-% % cD = Miller({1,1,0},crystalSystem.parent,'uvw');
-%% Define a sample direction tilted 60° from ND towards TD
-% % rotN = rotation('Euler',90*degree,60*degree,0*degree);
-% % sD = rotN * ND;
+%% Options:
+%  halfwidth    - halfwidth for the ODF calculation
+%  points       - number of points (discrete orientations) in the VPSC file
+%  export       - (optional path) and name of the VPSC file
 %%
-hwidth = get_option(varargin,'halfwidth',2.5*degree);
-numPoints = get_option(varargin,'points',1000);
-pfName_Out = get_option(varargin,'export','inputVPSC.Tex');
 
-%--- Define specimen symmetry
+hwidth = get_option(varargin,'halfwidth',2.5*degree);
+
+% define the specimen symmetry to compute ODF
 ss = specimenSymmetry('triclinic');
 
-%--- Calculate a fibre ODF
-odf = fibreODF(crystalDirection,specimenDirection,ss,'de la Vallee Poussin',...
-    'halfwidth',hwidth,'Fourier',22);
-%--- Define the ODF specimen symmetry
-odf.SS = specimenSymmetry('orthorhombic');
-%--- Save a VPSC *.tex file
-export_VPSC(odf,pfName_Out,'interface','VPSC','Bunge','points',numPoints);
-%---
+% check for MTEX version
+currentVersion = 5.9;
+fid = fopen('VERSION','r');
+MTEXversion = fgetl(fid);
+fclose(fid);
+MTEXversion = str2double(MTEXversion(5:end-2));
 
+if MTEXversion >= currentVersion % for MTEX versions 5.9.0 and above
+    pfName_Out = get_option(varargin,'export','inputFibre.txt');
+
+    % pre-define the fibre
+    f = fibre(symmetrise(crystalDirection),sampleDirection,ss,'full');
+
+    % calculate a fibre ODF
+    odf = fibreODF(f,'halfwidth',hwidth);
+    % re-define the ODF specimen symmetry based on user specification
+    odf.SS = sampleSymmetry;
+    % find the current working directory
+    dataPath = [pwd,'\'];
+    % define the path and file name
+    pfname = fullfile(dataPath,pfName_Out);
+    % save an MTEX ASCII File *.txt file (lossless format)
+    export(odf,pfname,'Bunge');
+
+else % for MTEX versions 5.8.2 and below
+    pfName_Out = get_option(varargin,'export','inputFibre.Tex');
+
+    % calculate a fibre ODF
+    odf = fibreODF(symmetrise(crystalDirection),sampleDirection,ss,'de la Vallee Poussin',...
+        'halfwidth',hwidth,'Fourier',22);
+    % re-define the ODF specimen symmetry based on user specification
+    odf.SS = sampleSymmetry;
+    % discretise the ODF (lossy format)
+    ori =  odf.discreteSample(length(odf.components{1}.weights));
+    % save an MTEX ASCII File *.txt file 
+    export(ori,pfName_Out,'Bunge');
+end
 end
