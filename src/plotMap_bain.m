f491495707de7a47880c21bee4218b29d859cd04
azdiarGazder <azdiar.gazder@gmail.com>
2023-05-09 10:45:37 +1000

    Update plotMap_bain.m

--- src/plotMap_bain.m
+++ src/plotMap_bain.m
@@ -22,7 +22,7 @@ setInterp2Latex
 
 p2c_V = job.p2c.variants;
 p2c_V = p2c_V(:);
-maxNrBains = max(job.transformedGrains.bainId);
+maxColors = max(job.transformedGrains.bainId);
 
 f = figure;
 if check_option(varargin,'grains')
@@ -35,8 +35,8 @@ else
     cEBSD = cEBSD(isParent);
     [~,~,bainIds] = calcVariantId(pGrains.meanOrientation,cEBSD.orientations,job.p2c,'variantMap',job.variantMap,varargin{:});
     plot(cEBSD,bainIds);
-    f_area = [histcounts(bainIds,maxNrBains)/length(bainIds)]';
-    disp(table([1:maxNrBains]',f_area,'VariableNames',{'Bain groups','AreaFrac'}))
+    f_area = [histcounts(bainIds,maxColors)/length(bainIds)]';
+    disp(table([1:maxColors]',f_area,'VariableNames',{'Bain groups','Area fraction'}))
 end
 
 
@@ -46,12 +46,11 @@ plot(parentGrains.boundary,varargin{:})
 hold off
 
 % Define the maximum number of color levels and plot the colorbar
-
 colormap(cmap);
-caxis([1 maxNrBains]);
+caxis([1 maxColors]);
 colorbar('location','eastOutSide','LineWidth',1.25,'TickLength', 0.01,...
-    'YTick', [1:1:maxNrBains],...
-    'YTickLabel',num2str([1:1:maxNrBains]'), 'YLim', [1 maxNrBains],...
+    'YTick', [1:1:maxColors],...
+    'YTickLabel',num2str([1:1:maxColors]'), 'YLim', [1 maxColors],...
     'TickLabelInterpreter','latex','FontName','Helvetica','FontSize',14,'FontWeight','bold');
 set(f,'Name','Bain group Id map','NumberTitle','on');
 drawnow;
