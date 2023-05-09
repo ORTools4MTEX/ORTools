11a09751ad75b24e857972f9dd4a8bcd6ce1d365
azdiarGazder <azdiar.gazder@gmail.com>
2023-05-09 10:45:29 +1000

    Update computeBainGrains.m

--- src/computeBainGrains.m
+++ src/computeBainGrains.m
@@ -13,19 +13,22 @@ function bains = computeBainGrains(job, varargin)
 gB = job.grainsPrior.boundary;
 % Only get child to child boundaries
 gB = gB(job.csChild,job.csChild);
-% Get packet Ids
+% Get bain groups
 bainId = job.bainId;
-% Get the packet numbers of the grains at the boundaries
+% Get the bain groups of the grains at the boundaries
 ids = bainId(gB.grainId);
 % Check where they are identical
 isBain = ids(:,1) - ids(:,2) == 0;
-% Get the packet boundaries
+% Get the bain boundaries
 bainBoundary = gB(isBain);
 % Get bain groups
 [bains,parentId] = merge(job.grainsPrior,bainBoundary);
- pId = nan(length(bains),1);
+%  pId = nan(length(bains),1);
+% 
+% % Somebody help me remove the loop to speed up the function!
+% for ii = 1:length(bains)
+%     pId(ii) = median(bainId(bains(ii).id==parentId),"all","omitnan");
+% end
 
-%Somebody help me remove the loop to speed up the function!
-for ii=1:length(bains)
-    pId(ii) = median(bainId(bains(ii).id==parentId),"all","omitnan");
-end
+pId = arrayfun(@(idx) median(bainId(bains(idx).id == parentId(idx)),'all','omitnan'), 1:length(bains));
+pId = reshape(pId, [], 1);
