4ffd5d8ea99f47111b7671772ebad0899bb7be76
azdiarGazder <azdiar.gazder@gmail.com>
2023-06-30 12:07:30 +1000

    Update computeHabitPlane.m

--- src/computeHabitPlane.m
+++ src/computeHabitPlane.m
@@ -402,6 +402,7 @@ end
 
 
 
+
 %% ANCILLARY FUNCTIONS
 
 %% Set Display Style of Miller objects
@@ -469,21 +470,17 @@ end
 %% Sub-divide a default colormap palette into a user specified number of
 % discrete colors to improve on the visual distinction between bins/levels.
 function outcmap = discreteColormap(incmap,nbins)
-
 % check for input bin size
 if length(incmap) < nbins
     error('Maximum number of bins exceeded.');
     return;
 end
-
 % generate a linearly spaced [n x 1] vector of row indices based on:
 % an interval = 1:maximum number of rows as the input colormap
 % the spacing between the points = (length(incmap)-1)/(nbins-1)
 out = [linspace(1,length(incmap),nbins)]';
-
 % convert the row indices between 2:end-1 to integers
 out(2:end-1,1) = round(out(2:end-1,1));
-
 % re-assign the row indices to the colormap
 outcmap = incmap(out,:);
 end
