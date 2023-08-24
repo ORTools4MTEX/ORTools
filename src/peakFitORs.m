f24d3c38a9fd017a71b975c581192596cb4c4218
azdiarGazder <azdiar.gazder@gmail.com>
2023-08-24 11:18:04 +1000

    Update peakFitORs.m

--- src/peakFitORs.m
+++ src/peakFitORs.m
@@ -20,19 +20,7 @@ screenPrint('Step','Computing ORs from peak-fitted data');
 ss = specimenSymmetry('triclinic');
 numORs = length(misoRange.min);
 
-%% Method selection
-% check for MTEX version
-currentVersion = 5.9;
-fid = fopen('VERSION','r');
-MTEXversion = fgetl(fid);
-fclose(fid);
-MTEXversion = str2double(MTEXversion(5:end-2));
-
-if MTEXversion >= currentVersion % for MTEX versions 5.9.0 and above
-    methodTypes = {'Maximum f(g)','Cancel'};
-else % for MTEX versions 5.8.2 and below
-    methodTypes = {'Maximum f(g)','Modal center','Cancel'};
-end
+methodTypes = {'Maximum f(g)','Cancel'};
 
 try
     methodSelected = questdlg('Choose the method to compute the OR(s):', ...
@@ -42,14 +30,6 @@ end
 % This command prevents function execution in cases when
 % the user presses the "Cancel" or "Close" buttons
 if isempty(methodSelected)
-    message = sprintf('Script terminated: Execution aborted by user');
-    uiwait(warndlg(message));
-    return
-elseif MTEXversion >= currentVersion && strcmpi(methodSelected,methodTypes{2})
-    message = sprintf('Script terminated: Execution aborted by user');
-    uiwait(warndlg(message));
-    return
-elseif MTEXversion < currentVersion && strcmpi(methodSelected,methodTypes{3})
     message = sprintf('Script terminated: Execution aborted by user');
     uiwait(warndlg(message));
     return
@@ -76,25 +56,8 @@ for jj = 1:numORs
         'resolution',0.25*degree,...
         'exact','silent');
 
-    if MTEXversion >= currentVersion % for MTEX versions 5.9.0 and above
         [~,mdfCenterParent] = max(mdfParent,...
             'accuracy',0.25*degree);
-
-    else % for MTEX versions 5.8.2 and below
-        if strcmpi(methodSelected,methodTypes{1})
-            [~,mdfCenterParent] = max(mdfParent,...
-                'FundamentalRegion',...
-                'resolution',0.25*degree,...
-                'silent');
-
-        elseif strcmpi(methodSelected,methodTypes{2})
-            mdfCenterParent = calcModes(mdfParent,...
-                'FundamentalRegion',...
-                'resolution',0.25*degree,...
-                'silent');
-            %         'accuracy',0.05*degree,...
-        end
-    end
 
     mdfAxisParent = mdfCenterParent.axis(job.csParent.mineral,job.csParent.mineral,ss);
     p2c(jj) = mdfCenterParent;
