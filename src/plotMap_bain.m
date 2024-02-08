97adb9261df377f07e4229489493f390d92d79c3
Frank Nießen <frannie@dtu.dk>
2024-02-08 13:15:31 +0100

    Added BC overlay plotting

--- src/plotMap_bain.m
+++ src/plotMap_bain.m
@@ -17,27 +17,55 @@ function f_area = plotMap_bain(job, varargin)
 %  colormap - colormap variable
 %  grains   - plot grain data instead of EBSD data
 
-set(0,'DefaultFigureVisible','off');
-cmap = get_option(varargin,'colormap',magma);
-allfigh = findall(0,'type','figure');
-if length(allfigh) > 1
-    close(figure(length(allfigh)+1));
-else
-    close(figure(1));
-end
-set(0,'DefaultFigureVisible','on');
+% set(0,'DefaultFigureVisible','off');
+% allfigh = findall(0,'type','figure');
+% if length(allfigh) > 1
+%     close(figure(length(allfigh)+1));
+% else
+%     close(figure(1));
+% end
+% set(0,'DefaultFigureVisible','on');
 
 %% Define the text output format as Latex
 setInterp2Latex
+cmap = get_option(varargin,'colormap',rdylgn);
+facealpha = get_option(varargin,'facealpha',0.6);
 
-p2c_V = job.p2c.variants;
-p2c_V = p2c_V(:);
-maxColors = max(job.transformedGrains.bainId);
+% Scale colormap
+nr_bain = max(job.transformedGrains.bainId);
+if nr_bain < 8
+    n1 = round((0:(nr_bain-1)) ./ (nr_bain-1) .* ...
+        (size(cmap,1) - 1)) + 1;
+    cmap = cmap(n1, :);
+else
+    n1 = floor(size(cmap, 1) / nr_bain); % interval between colormap indices
+    n2 = length(cmap) - nr_bain*n1 + 1;   % starting index, i.e. mod(cm,n) does not always equal 0
+    cmap = cmap(n2:n1:end, :); 
+end
 
 f = figure();
-hold all
+if check_option(varargin,'bc')
+    if isfield(job.ebsdPrior.prop,'bc')
+        prop = rescale(filloutliers(job.ebsdPrior.prop.bc,"clip",'quartiles'));
+    elseif isfield(job.ebsdPrior.prop,'iq')
+        prop = rescale(filloutliers(job.ebsdPrior.prop.iq,"clip",'quartiles'));
+    else
+        warning('BC was not plotted, as data is missing.');
+    end
+
+    % Create main axis and then copy it.  
+    plt1 = plot(job.ebsdPrior,prop);
+else
+    plt1 = plot(job.ebsdPrior,nan(size(job.ebsdPrior)));
+    
+end
+ax1 = plt1.Parent; 
+ax2 = copyobj(ax1,f);
+delete(ax2.Children);
+colormap(ax1,'gray')
+
 if check_option(varargin,'grains')
-    plot(job.transformedGrains,job.transformedGrains.bainId);
+     plt2 = plot(job.transformedGrains,job.transformedGrains.bainId,'parent',ax2);
 else
     pGrains = job.grains(job.mergeId(job.ebsdPrior(job.csChild).grainId));
     isParent = pGrains.phaseId == job.parentPhaseId;
@@ -45,24 +73,36 @@ else
     cEBSD = job.ebsdPrior(job.csChild);
     cEBSD = cEBSD(isParent);
     [~,~,bainIds] = calcVariantId(pGrains.meanOrientation,cEBSD.orientations,job.p2c,'variantMap',job.variantMap,varargin{:});
-    plot(cEBSD,bainIds);
-    f_area = [histcounts(bainIds,maxColors)/length(bainIds)]';
-    disp(table([1:maxColors]',f_area,'VariableNames',{'BainGroups','AreaFrac'}))
+    plt2 = plot(cEBSD,bainIds,'parent',ax2);
+    f_area = [histcounts(bainIds,nr_packets)/length(bainIds)]';
+    disp(table([1:nr_packets]',f_area,'VariableNames',{'Bain Groups','AreaFrac'}))
 end
 
 
-hold all
+if check_option(varargin,'bc')
+    set(plt2,'facealpha',facealpha);
+end
+colormap(ax2,cmap)
+
+% Link the axis properties and turn off axis #2.
+ax2.UserData = linkprop([ax1,ax2],...
+    {'Position','InnerPosition','DataAspectRatio','xtick','ytick', ...
+    'ydir','xdir','xlim','ylim'});
+ax2.Visible = 'off';
+ax2.Color = 'none';
+
+% Plot parent grain boundaries
+hold on
 parentGrains = smooth(job.parentGrains,10);
 plot(parentGrains.boundary,varargin{:})
 hold off
 
 % Define the maximum number of color levels and plot the colorbar
-colormap(cmap);
-caxis([1 maxColors]);
-colorbar('location','eastOutSide','LineWidth',1.25,'TickLength', 0.01,...
-    'YTick', [1:1:maxColors],...
-    'YTickLabel',num2str([1:1:maxColors]'), 'YLim', [1 maxColors],...
+
+colorbar(ax2,'location','eastOutSide','LineWidth',1.25,'TickLength', 0.01,...
+    'YTick', [1:1:nr_bain],'YTickLabel',string(num2str([1:1:nr_bain]')), 'YLim', [0.5 nr_bain+0.5],...
     'TickLabelInterpreter','latex','FontName','Helvetica','FontSize',14,'FontWeight','bold');
+clim(ax2,[0.5 nr_bain + 0.5]);
 set(f,'Name','Bain group Id map','NumberTitle','on');
 drawnow;
 end
--- src/plotMap_packets.m
+++ src/plotMap_packets.m
@@ -18,18 +18,48 @@ function f_area = plotMap_packets(job, varargin)
 %  colormap - colormap variable
 %  grains   - plot grain data instead of EBSD data
 
-cmap = get_option(varargin,'colormap',viridis);
-
 %% Define the text output format as Latex
 setInterp2Latex
+cmap = get_option(varargin,'colormap',viridis);
+facealpha = get_option(varargin,'facealpha',0.6);
 
-p2c_V = job.p2c.variants;
-p2c_V = p2c_V(:);
-maxPackets = max(job.transformedGrains.packetId);
+% Scale colormap
+nr_packets = max(job.transformedGrains.packetId);
+if nr_packets < 8
+    n1 = round((0:(nr_packets-1)) ./ (nr_packets-1) .* ...
+        (size(cmap,1) - 1)) + 1;
+    cmap = cmap(n1, :);
+else
+    n1 = floor(size(cmap, 1) / nr_packets); % interval between colormap indices
+    n2 = length(cmap) - nr_packets*n1 + 1;   % starting index, i.e. mod(cm,n) does not always equal 0
+    cmap = cmap(n2:n1:end, :); 
+end
 
-f = figure;
+% Start plotting
+
+f = figure();
+if check_option(varargin,'bc')
+    if isfield(job.ebsdPrior.prop,'bc')
+        prop = rescale(filloutliers(job.ebsdPrior.prop.bc,"clip",'quartiles'));
+    elseif isfield(job.ebsdPrior.prop,'iq')
+        prop = rescale(filloutliers(job.ebsdPrior.prop.iq,"clip",'quartiles'));
+    else
+        warning('BC was not plotted, as data is missing.');
+    end
+
+    % Create main axis and then copy it.  
+    plt1 = plot(job.ebsdPrior,prop);
+else
+    plt1 = plot(job.ebsdPrior,nan(size(job.ebsdPrior)));
+    
+end
+ax1 = plt1.Parent; 
+ax2 = copyobj(ax1,f);
+delete(ax2.Children);
+colormap(ax1,'gray')
+
 if check_option(varargin,'grains')
-    plot(job.transformedGrains,job.transformedGrains.packetId);
+     plt2 = plot(job.transformedGrains,job.transformedGrains.packetId,'parent',ax2);
 else
     pGrains = job.grains(job.mergeId(job.ebsdPrior(job.csChild).grainId));
     isParent = pGrains.phaseId == job.parentPhaseId;
@@ -37,12 +67,24 @@ else
     cEBSD = job.ebsdPrior(job.csChild);
     cEBSD = cEBSD(isParent);
     [~,packIds,~] = calcVariantId(pGrains.meanOrientation,cEBSD.orientations,job.p2c,'variantMap',job.variantMap,varargin{:});
-    plot(cEBSD,packIds);
-    f_area = [histcounts(packIds,maxPackets)/length(packIds)]';
-    disp(table([1:maxPackets]',f_area,'VariableNames',{'Packets','AreaFrac'}))
+    plt2 = plot(cEBSD,packIds,'parent',ax2);
+    f_area = [histcounts(packIds,nr_packets)/length(packIds)]';
+    disp(table([1:nr_packets]',f_area,'VariableNames',{'Packets','AreaFrac'}))
 end
 
+if check_option(varargin,'bc')
+    set(plt2,'facealpha',facealpha);
+end
+colormap(ax2,cmap)
 
+% Link the axis properties and turn off axis #2.
+ax2.UserData = linkprop([ax1,ax2],...
+    {'Position','InnerPosition','DataAspectRatio','xtick','ytick', ...
+    'ydir','xdir','xlim','ylim'});
+ax2.Visible = 'off';
+ax2.Color = 'none';
+
+% Plot parent grain boundaries
 hold on
 parentGrains = smooth(job.parentGrains,10);
 plot(parentGrains.boundary,varargin{:})
@@ -50,12 +92,10 @@ hold off
 
 % Define the maximum number of color levels and plot the colorbar
 
-colormap(cmap);
-caxis([1 maxPackets]);
-colorbar('location','eastOutSide','LineWidth',1.25,'TickLength', 0.01,...
-    'YTick', [1:1:maxPackets],...
-    'YTickLabel',num2str([1:1:maxPackets]'), 'YLim', [1 maxPackets],...
+colorbar(ax2,'location','eastOutSide','LineWidth',1.25,'TickLength', 0.01,...
+    'YTick', [1:1:nr_packets],'YTickLabel',string(num2str([1:1:nr_packets]')), 'YLim', [0.5 nr_packets+0.5],...
     'TickLabelInterpreter','latex','FontName','Helvetica','FontSize',14,'FontWeight','bold');
+clim(ax2,[0.5 nr_packets + 0.5]);
 set(f,'Name','Packet Id map','NumberTitle','on');
 drawnow;
 end
